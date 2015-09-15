289c8ea260ab26f207bd5b67e1f20b3d72f51bbc
Sam Harada <sam@antumbra.io>
2015-09-15 00:01:21 -0400

    Initial commit (it doesn't do anything)

---
+++ __main__.py
@@ -0,0 +1,3 @@
+import lintumbra
+
+lintumbra.main()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='lintumbra',
+      version='0.0.1',
+      description="Antumbra Glow client for Linux",
+      author="Team Antumbra",
+      author_email='contact@antumbra.io',
+      url='https://github.com/TeamAntumbra/lintumbra',
+      packages=['lintumbra'],
+      package_dir={'lintumbra': '.'},
+      scripts=['lintumbra'])
